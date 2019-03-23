7dfe37d507799648f7e9dea4cbe6710ba774a498
punisher <skg44@njit.edu>
2019-03-23 19:20:02 -0400

    Fixed comment and removed extra space

--- js/gallery.js
+++ js/gallery.js
@@ -31,31 +31,29 @@ function animate() {
 }
 
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
+
 // Counter for the mImages array
 let mCurrentIndex = 0;
-
-
-
 function swapPhoto() {
 	//Add code here to access the #slideShow element.
 	//Access the img element and replace its source
 	//with a new image from your images array which is loaded 
 	//from the JSON string
 
-    // The if statement will check the length of mImages, and if the currentIndext is < than '0' it will increment
+    // The if statement will check the length of mImages, and if the currentIndex is < than '0' it will increment
     if(mCurrentIndex < 0){
         mCurrentIndex += mImages.length;
     }
 
     // The attr() method will set and return attributes and values of the selected elements
-    $("#photo").attr('src', mImages[mCurrentIndex].path);
+    $("#photo").attr('src', mImages[mCurrentIndex].imgPath);
 
     // Ex: .location is a class accessing from HTML file
     // .text is a property that will set or return text value
     // Ex: It will get the text from the image file and will display on web based on the image is selected
-    $(".location").text('Location: '+mImages[mCurrentIndex].location);
-    $(".description").text('Description: '+mImages[mCurrentIndex].description);
-    $(".date").text('Date: '+mImages[mCurrentIndex].date);
+    $(".imgLocation").text("Location: "+mImages[mCurrentIndex].imgLocation);
+    $(".description").text("Description: "+mImages[mCurrentIndex].description);
+    $(".date").text("Date: "+mImages[mCurrentIndex].date);
 
     mCurrentIndex++;
     if(mCurrentIndex >= mImages.length){
@@ -64,6 +62,27 @@ function swapPhoto() {
 
 	console.log('swap photo');
 }
+
+// Array holding GalleryImage objects (see below).
+let mImages = [];
+
+// Holds the retrieved JSON information
+let mJson;
+
+
+
+// Requesting $_GET variable
+let $_GET = getQueryLocation(document.location.search);
+// URL for the JSON to load by default
+// Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
+// The if and else statement checks if either one json file become true
+let mUrl;
+if($_GET["json"] === undefined) {
+    mUrl = "images.short.json";
+} else {
+    mUrl = $_GET["json"];
+}
+
 
 // XMLHttpRequest variable
 // Property contains the event handler to be called when the readystatechange event is fired
@@ -84,8 +103,8 @@ mRequest.onreadystatechange = function() {
             // mJson.images[i] is an array which starts from 0
             // Push() add thing
             for (let i = 0; i < mJson.images.length; i++) {
-                mImages.push(new GalleryImage(mJson.images[i].location, mJson.images[i].description,
-                    mJson.images[i].date, mJson.images[i].path));
+                mImages.push(new GalleryImage(mJson.images[i].imgLocation, mJson.images[i].description,
+                    mJson.images[i].date, mJson.images[i].imgPath));
             }
 
             // the catch will check for error and will display message,
@@ -96,31 +115,10 @@ mRequest.onreadystatechange = function() {
     }
 };
 
-// The XMLHttpRequest method open() initializes a newly-created request, or re-initializes an existing one.
+// The XMLHttpRequest method open() initializes a newly-created request or re-initializes an existing one.
 mRequest.open("GET", mUrl, true);
 // The XMLHttpRequest method send() the request to the server
 mRequest.send();
-
-
-// Array holding GalleryImage objects (see below).
-let mImages = [];
-
-// Holds the retrived JSON information
-let mJson;
-
-
-
-// Requesting $_GET variable
-let $_GET = getQueryLocation(document.location.search);
-// URL for the JSON to load by default
-// Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-// The if and else statement checks if either one json file become true
-let mUrl;
-if($_GET["json"] == undefined) {
-    mUrl = "images.short.json";
-} else {
-    mUrl = $_GET["json"];
-}
 
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
@@ -133,33 +131,33 @@ function makeGalleryImageOnloadCallback(galleryImage) {
 }
 
 
-
 $(document).ready( function() {
-	
-	// This initially hides the photos' metadata information
-	$('.details').eq(0).hide();
+    //this initially hides the photos' metadata information
+    let jQuery = $('.details').eq(0).hide();
 
-	// When click on the arrow the indicator will rotate 90 horizontally
+    // When click on the arrow the indicator will rotate 90 horizontally
     // The click() method attaches an event handler function to an HTML element
     // The function will is executed when the user clicks on the HTML element
-	$('.moreIndicator').onclick(function () {
+    $(".moreIndicator").click(function(){
 
-	    // The toggleClass() method toggles the arrow. Like turn it in on and off
-	    $("img.rot90").toggleClass("rot270", 3000);
-	    // The slideToggle method is used to switch between the slideup and slidedown states.
-	    $(".details").slideToggle(1000);
+        // The toggleClass() method toggles the arrow. Like turn it on and off
+        $( "img.rot90" ).toggleClass("rot270",3000);
+        // The slideToggle method is used to switch between the slideup and slidedown details.
+        $(".details").slideToggle(1000);
     });
 
-	// swapPhoto to next
-	$("#nextPhoto").onclick(function () {
+    // swapPhoto to next
+    $("#nextPhoto").click(function(){
         swapPhoto();
+
     });
 
-	$("#prevPhoto").onclick(function () {
-       mCurrentIndex -= 2;
-       swapPhoto();
-       console.log(mCurrentIndex);
+    $("#prevPhoto").click(function(){
+        mCurrentIndex -= 2;
+        swapPhoto();
+        console.log(mCurrentIndex);
     });
+
 });
 
 
@@ -168,15 +166,15 @@ window.addEventListener('load', function() {
 	console.log('window loaded');
 }, false);
 
-function GalleryImage(location, description, date, path) {
+function GalleryImage(imgLocation, description, date, imgPath) {
 	//implement me as an object to hold the following data about an image:
 	//1. location where photo was taken
 	//2. description of photo
 	//3. the date when the photo was taken
 	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
 
-	this.location = location;
+	this.imgLocation = imgLocation;
 	this.description = description;
 	this.date = date;
-	this.path = path;
+	this.imgPath = imgPath;
 }
